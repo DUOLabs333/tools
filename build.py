0148edb80c3265c51ddac5c2e822a619e946f1b9
DUOLabs333 <dvdugo333@gmail.com>
2024-07-04 01:17:29 -0400

    Start working on the build tool

---
+++ build.py
@@ -0,0 +1,19 @@
+output_name
+
+build() for those that do something else
+
+clean as a target
+
+debug_flags
+
+release_flags
+
+clean is a target, but debug is still an env variable
+
+for the actual vulkan-stream
+
+have a base class, where thw only thing that differs is whether CLIENT is 0 or 1
+
+offer PLATFORM, DEBUG, and EXTERNAL_DIR as global variables
+
+classes should just be a child of a base class
